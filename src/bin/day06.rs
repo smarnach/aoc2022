15102f5acf4e37a5046e470ff6277a56b43b1300
Sven Marnach <sven@mozilla.com>
2022-12-06 11:37:21 +0100

    06: Use bit sets and short-circuiting.

--- src/bin/day06.rs
+++ src/bin/day06.rs
@@ -1,20 +1,36 @@
-use std::collections::HashSet;
-
-use anyhow::{Context, Result};
+use anyhow::{Error, Result};
 use aoc2022::read_input;
 
 fn main() -> Result<()> {
     let input = read_input!()?;
-    let sop = start_marker(input.as_bytes(), 4).context("no start of packet found")?;
-    println!("{sop}");
-    let som = start_marker(input.as_bytes(), 14).context("no start of message found")?;
-    println!("{som}");
+    let packet = parse_input(&input)?;
+    println!("{}", start_pos(packet, 4)?);
+    println!("{}", start_pos(packet, 14)?);
     Ok(())
 }
 
-fn start_marker(packet: &[u8], len: usize) -> Option<usize> {
-    packet
-        .windows(len)
-        .position(|win| HashSet::<_>::from_iter(win).len() == len)
-        .map(|i| i + len)
+fn start_pos(packet: &[u8], len: usize) -> Result<usize> {
+    let mut i = 0;
+    'outer: while i < packet.len() - len {
+        let mut set = 0u32;
+        for j in (i..i + len).rev() {
+            let mask = 1 << (packet[j] - b'a');
+            if set & mask != 0 {
+                i = j + 1;
+                continue 'outer;
+            }
+            set |= mask;
+        }
+        return Ok(i + len);
+    }
+    Err(Error::msg("no start marker found"))
+}
+
+fn parse_input(input: &str) -> Result<&[u8]> {
+    let packet = input.trim().as_bytes();
+    if packet.iter().all(u8::is_ascii_lowercase) {
+        Ok(packet)
+    } else {
+        Err(Error::msg("invalid character in input"))
+    }
 }
